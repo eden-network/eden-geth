90ad6877d0b2ba37f6bcfc6dcea87222de9ce757
Jeffrey Quesnelle <jeff@edennetwork.io>
2021-02-19 10:30:30 -0800

    add Eden

---
+++ miner/metrics.go
@@ -0,0 +1,29 @@
+// Copyright 2021 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package miner
+
+import (
+	"github.com/ethereum/go-ethereum/metrics"
+)
+
+var (
+	taskSwitches           = metrics.NewRegisteredCounter("miner/task/switches", nil)
+	taskSwitchPerBlock     = metrics.NewRegisteredGauge("miner/task/switchesPerBlock", nil)
+	taskActiveTimer        = metrics.NewRegisteredTimer("miner/task/active", nil)
+	taskFlashbotsWorker    = metrics.NewRegisteredGauge("miner/task/flasbots/worker", nil)
+	taskEdenSlotTxsInBlock = metrics.NewRegisteredGauge("miner/task/eden/slotTxsInBlock", nil)
+)
